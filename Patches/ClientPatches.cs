58c221c13b58baf4fe53f9aae13fbd1e5ba85efd
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-07-06 09:42:05 -0500

    Lets add some of the cool logging and other things

---
+++ Patches/ClientPatches.cs
@@ -0,0 +1,271 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using BepInEx;
+using BepInEx.Bootstrap;
+using HarmonyLib;
+using Steamworks;
+using UnityEngine;
+
+namespace VMP_Mod
+{
+	public class ClientPatches
+	{
+
+		[HarmonyPatch(typeof(Ship), "Awake")]
+		public static class shipfix
+		{
+			private static void Postfix(ref Ship __instance)
+			{
+				__instance.m_minWaterImpactForce = 100f;
+			}
+		}
+
+		[HarmonyPatch(typeof(WearNTear), "RPC_Damage")]
+		public static class DamageWard
+		{
+			private static bool Prefix(long sender, HitData hit, WearNTear __instance)
+			{
+				bool result = true;
+				if (hit != null && hit.GetAttacker() != null && hit.GetAttacker().IsPlayer() && __instance.gameObject != null)
+				{
+					Player player = (Player)hit.GetAttacker();
+					if (__instance.gameObject.name.Contains("piece_chest") || __instance.gameObject.name.Contains("guard_stone"))
+					{
+						foreach (PrivateArea allArea in PrivateArea.m_allAreas)
+						{
+							if (allArea.IsEnabled() && allArea.IsInside(hit.m_point, 1f))
+							{
+								if (player.GetPlayerName().ToUpper().Contains("ADMIN"))
+								{
+									result = true;
+								}
+								else if (allArea.m_piece.m_creator != player.GetPlayerID() && !allArea.IsPermitted(player.GetPlayerID()))
+								{
+									allArea.FlashShield(flashConnected: false);
+									result = false;
+								}
+							}
+						}
+					}
+				}
+				return result;
+			}
+		}
+
+		[HarmonyPatch(typeof(Humanoid), "EquipItem")]
+		public static class PlayerEquip
+		{
+			private static void Postfix(ItemDrop.ItemData item, bool triggerEquipEffects, ref Humanoid __instance)
+			{
+				playerEqp(ref item, ref __instance);
+			}
+		}
+
+		[HarmonyPatch(typeof(Humanoid), "UnequipItem")]
+		public static class PlayerUnEquip
+		{
+			private static void Postfix(ItemDrop.ItemData item, bool triggerEquipEffects, ref Humanoid __instance)
+			{
+				playerEqp(ref item, ref __instance);
+			}
+		}
+
+		[HarmonyPatch(typeof(Player), "ConsumeItem")]
+		public static class ConsumeLog
+		{
+			private static void Postfix(Inventory inventory, ItemDrop.ItemData item, Player __instance)
+			{
+				if (__instance != null && item != null && item.m_shared != null && __instance.m_nview.IsValid())
+				{
+					string text = ((item.m_crafterID == 0L) ? " : uncrafted : " : (" : crafted by : " + item.m_crafterName + " "));
+					SendModerationLog(__instance.GetPlayerName() + " : consume : " + item.m_shared.m_name + text);
+				}
+			}
+		}
+
+
+		[HarmonyPatch(typeof(Player), "SetIntro")]
+		public static class SpawnLog
+		{
+			private static void Postfix(bool intro, Player __instance)
+			{
+				if (!ZNet.instance.IsServer() && __instance != null && __instance.m_nview.IsValid() && ZRoutedRpc.instance != null)
+				{
+					ZLog.LogWarning("FRESH " + __instance.GetPlayerName());
+					SendModerationLog(__instance.GetPlayerName() + " : FRESH CHARACTER");
+					ZRoutedRpc.instance.InvokeRoutedRPC("ServerAddFresh", __instance.GetPlayerName());
+				}
+			}
+		}
+
+        [HarmonyPatch(typeof(ZSteamMatchmaking))]
+        [HarmonyPatch("GetServers")]
+        private class PatchUpdateServerListGui
+        {
+            private static void Postfix(ref List<ServerData> allServers)
+            {
+                ServerData serverData = new ServerData();
+                serverData.m_host = "23.23.255.113";
+                serverData.m_name = "VMP A PVE Server";
+                serverData.m_password = false;
+                serverData.m_players = 100;
+                serverData.m_port = 2456;
+                serverData.m_steamHostID = 0uL;
+                serverData.m_steamHostAddr = default(SteamNetworkingIPAddr);
+                serverData.m_steamHostAddr.ParseString(serverData.m_host + ":" + serverData.m_port);
+                serverData.m_upnp = true;
+                serverData.m_version = "";
+                allServers.Insert(0, serverData);
+            }
+        }
+
+
+		[HarmonyPatch(typeof(WearNTear), "OnPlaced")]
+		private class shipCreated
+		{
+			private static void Postfix(ref WearNTear __instance)
+			{
+				if ((bool)__instance)
+				{
+					Ship component = __instance.gameObject.GetComponent<Ship>();
+					if ((bool)component && (bool)component.m_nview && component.m_nview.IsValid())
+					{
+						component.m_nview.GetZDO().Set("creatorName", Game.instance.GetPlayerProfile().GetName());
+						SendModerationLog("ship created " + component.gameObject.name + " " + Game.instance.GetPlayerProfile().GetName());
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(Ship), "OnDestroyed")]
+		private class shipDestroyed
+		{
+			private static void Postfix(ref Ship __instance)
+			{
+				if (__instance.IsOwner() && (bool)__instance.m_nview && __instance.m_nview.IsValid())
+				{
+					List<Player> list = new List<Player>();
+					Player.GetPlayersInRange(__instance.transform.position, 20f, list);
+					SendModerationLog("ship destroyed " + __instance.gameObject.name + " creator: " + __instance.m_nview.GetZDO().GetString("creatorName") + " around: " + string.Join(",", list.Select((Player p) => p.GetPlayerName())));
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(Player), "StartShipControl")]
+		private class shipControlled
+		{
+			private static void Postfix(ref ShipControlls shipControl)
+			{
+				SendModerationLog("ship controlled " + shipControl.GetShip().gameObject.name + " creator: " + shipControl.GetShip().m_nview.GetZDO().GetString("creatorName") + " player: " + Game.instance.GetPlayerProfile().GetName());
+			}
+		}
+
+		[HarmonyPatch(typeof(Game), "SpawnPlayer")]
+		private class respawnclass
+		{
+			private static void Postfix()
+			{
+				try
+				{
+					ZRoutedRpc.instance.Register<string>("ClientSetClass", RPC_SetClass);
+				}
+				catch (Exception)
+				{
+					ZLog.LogError("already registered ClientSetClass");
+				}
+				ZRoutedRpc.instance.InvokeRoutedRPC("ServerGetClass", Player.m_localPlayer.GetPlayerName());
+			}
+		}
+
+
+
+		[HarmonyPatch(typeof(ItemStand), "CanAttach")]
+		public class ItemStand_CanAttach
+		{
+			public static void Postfix(ItemStand __instance, ref bool __result)
+			{
+				if (__instance.m_name == "$piece_itemstand")
+				{
+					__result = true;
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(ItemStand), "GetAttachPrefab")]
+		public class ItemStand_GetAttachPrefab
+		{
+			public static void Postfix(GameObject item, ref GameObject __result)
+			{
+				if (__result == null)
+				{
+					Collider componentInChildren = item.transform.GetComponentInChildren<Collider>();
+					if ((bool)componentInChildren)
+					{
+						__result = componentInChildren.transform.gameObject;
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(VisEquipment), "EnableEquipedEffects")]
+		private class VariantColors
+		{
+			private static void Postfix(ref GameObject instance, ref VisEquipment __instance)
+			{
+				if ((bool)instance)
+				{
+					Player component = __instance.gameObject.GetComponent<Player>();
+					if (!(component != null))
+					{
+					}
+				}
+			}
+		}
+
+	
+
+		public static bool admin = false;
+
+		public static string playerClass = "";
+
+		public static bool Contains(string source, string toCheck, StringComparison comp)
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+			string text = source.ToUpperInvariant();
+			string value = toCheck.ToUpperInvariant();
+			return text.Contains(value);
+		}
+
+		public static void SendModerationLog(string message)
+		{
+			if (ZRoutedRpc.instance != null && !ZRoutedRpc.instance.m_server)
+			{
+				ZRoutedRpc.instance.InvokeRoutedRPC("ServerModerationLog", message);
+			}
+		}
+
+		public static void playerEqp(ref ItemDrop.ItemData item, ref Humanoid huma)
+		{
+			if (item != null && item.IsEquipable() && huma != null && huma.IsPlayer())
+			{
+				Player player = (Player)huma;
+				if (player.m_nview.IsValid() && Player.m_localPlayer != null)
+				{
+					string text = ((item.m_crafterID == 0L) ? " : uncrafted : " : (" : crafted by : " + item.m_crafterName + " "));
+				}
+			}
+		}
+
+		public static void RPC_SetClass(long sender, string vclass)
+		{
+			if (ZNet.instance != null && !ZNet.instance.IsServer())
+			{
+				ZLog.LogWarning("vmp got class from server " + vclass);
+				playerClass = vclass;
+			}
+		}
+	}
+}
---
+++ Patches/ServerPatches.cs
@@ -0,0 +1,283 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using HarmonyLib;
+
+namespace vrp.Patches
+{
+	internal class ServerPatches
+	{
+		[HarmonyPatch(typeof(ZNet), "Awake")]
+		public static class Znetawakfix
+		{
+			private static void Postfix()
+			{
+				if (ZNet.instance.IsServer())
+				{
+					fresh = new SyncedList(Utils.GetSaveDataPath() + "/fresh.txt", "List fresh players ID|name ONE per line");
+					classes = new SyncedList(Utils.GetSaveDataPath() + "/classes.txt", "List player classes ID|class ONE per line");
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(ZDOMan))]
+		[HarmonyPatch(MethodType.Constructor)]
+		[HarmonyPatch(new Type[] { typeof(int) })]
+		public static class AddCustomeRPCHere
+		{
+			private static void Postfix()
+			{
+				ZRoutedRpc.instance.Register<string>("ServerModerationLog", RPC_ModerationLog);
+				ZRoutedRpc.instance.Register<string>("ServerAddFresh", RPC_AddFresh);
+				ZRoutedRpc.instance.Register<string>("ServerGetClass", RPC_GetClass);
+			}
+		}
+
+		[HarmonyPatch(typeof(ZDOMan), "AddPeer")]
+		public static class PeerAdded
+		{
+			private static void Postfix(ZNetPeer netPeer, ZDOMan __instance)
+			{
+				RPC_ModerationLog(__instance.m_myid, netPeer.m_socket.GetHostName() + "|" + netPeer.m_playerName + " Connected");
+			}
+		}
+
+		[HarmonyPatch(typeof(ZDOMan), "RemovePeer")]
+		public static class PeerRemoved
+		{
+			private static void Postfix(ZNetPeer netPeer, ZDOMan __instance)
+			{
+				RPC_ModerationLog(__instance.m_myid, netPeer.m_socket.GetHostName() + "|" + netPeer.m_playerName + " Disconnected");
+			}
+		}
+
+		[HarmonyPatch(typeof(FejdStartup), "IsPublicPasswordValid")]
+		public static class Passwordfix
+		{
+			private static bool Prefix(string password, World world, ref bool __result, FejdStartup __instance)
+			{
+				__result = true;
+				return false;
+			}
+		}
+
+		[HarmonyPatch(typeof(ZNet), "IsAllowed")]
+		public static class CharWhitelist
+		{
+			private static bool Prefix(ref bool __result, string hostName, string playerName, ZNet __instance)
+			{
+				string text = hostName + "|" + playerName;
+				__result = !__instance.m_bannedList.Contains(hostName) && !__instance.m_bannedList.Contains(playerName) && (__instance.m_permittedList.Count() <= 0 || __instance.m_permittedList.Contains(text));
+				if (!__result)
+				{
+					RPC_ModerationLog(0L, "IsAllowed failed: " + text);
+				}
+				return false;
+			}
+		}
+
+		[HarmonyPatch(typeof(ZNet), "CheckWhiteList")]
+		public static class CheckCharWhitelist
+		{
+			private static bool Prefix(ZNet __instance)
+			{
+				if (__instance.m_permittedList != null && __instance.m_peers != null)
+				{
+					if (__instance.m_permittedList.Count() > 0 && __instance.m_peers.Count > 0)
+					{
+						foreach (ZNetPeer item in __instance.m_peers.ToList())
+						{
+							if (item.IsReady() && !item.m_characterID.IsNone())
+							{
+								string text = item.m_socket.GetHostName() + "|" + item.m_playerName;
+								ZDO zDO = __instance.m_zdoMan.GetZDO(item.m_characterID);
+								if (zDO != null && zDO.GetBool("dead") && !timers.ContainsKey(text))
+								{
+									RPC_ModerationLog(0L, text + "DEAD");
+									timers.Add(text, 5);
+								}
+								if (!__instance.m_permittedList.Contains(text))
+								{
+									ZLog.Log("Kicking player not in permitted list " + text);
+									RPC_ModerationLog(0L, "Kicking player not in permitted list " + text);
+									__instance.InternalKick(item);
+								}
+							}
+						}
+					}
+					foreach (string item2 in timers.Keys.ToList())
+					{
+						timers[item2] -= 1;
+						if (timers[item2] < 0)
+						{
+							timers.Remove(item2);
+						}
+					}
+				}
+				return false;
+			}
+		}
+
+		[HarmonyPatch(typeof(ZNet), "SaveWorldThread")]
+		public static class saferSaveWorldThread
+		{
+			private static bool Prefix(ZNet __instance)
+			{
+				try
+				{
+					DateTime now = DateTime.Now;
+					string dBPath = ZNet.m_world.GetDBPath();
+					string text = dBPath + ".new";
+					string text2 = dBPath + ".old" + now.ToString("HH");
+					Directory.CreateDirectory(ZNet.m_world.m_worldSavePath);
+					FileStream fileStream = File.Create(text);
+					BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+					binaryWriter.Write(Version.m_worldVersion);
+					binaryWriter.Write(__instance.m_netTime);
+					__instance.m_zdoMan.SaveAsync(binaryWriter);
+					ZoneSystem.instance.SaveASync(binaryWriter);
+					RandEventSystem.instance.SaveAsync(binaryWriter);
+					binaryWriter.Flush();
+					fileStream.Flush(flushToDisk: true);
+					binaryWriter.Close();
+					binaryWriter.Dispose();
+					fileStream.Close();
+					fileStream.Dispose();
+					Thread.Sleep(1000);
+					if (File.Exists(dBPath))
+					{
+						if (File.Exists(text2))
+						{
+							File.Delete(text2);
+							Thread.Sleep(1000);
+						}
+						File.Move(dBPath, text2);
+						Thread.Sleep(1000);
+					}
+					File.Move(text, dBPath);
+					ZNet.m_world.SaveWorldMetaData();
+					ZLog.Log("World saved ( " + (DateTime.Now - now).TotalMilliseconds + "ms )");
+				}
+				catch (Exception ex)
+				{
+					ZLog.Log("SaveWorldThread EXCEPTION");
+					ZLog.LogError(ex.Message);
+					ZLog.LogError(ex.StackTrace);
+				}
+				return false;
+			}
+		}
+
+		[HarmonyPatch(typeof(World), "SaveWorldMetaData")]
+		public static class saferSaveWorldMetaData
+		{
+			private static bool Prefix(World __instance)
+			{
+				try
+				{
+					Directory.CreateDirectory(__instance.m_worldSavePath);
+					ZPackage zPackage = new ZPackage();
+					zPackage.Write(Version.m_worldVersion);
+					zPackage.Write(__instance.m_name);
+					zPackage.Write(__instance.m_seedName);
+					zPackage.Write(__instance.m_seed);
+					zPackage.Write(__instance.m_uid);
+					zPackage.Write(__instance.m_worldGenVersion);
+					DateTime now = DateTime.Now;
+					string metaPath = __instance.GetMetaPath();
+					string text = metaPath + ".new";
+					string text2 = metaPath + ".old" + now.ToString("HH");
+					byte[] array = zPackage.GetArray();
+					FileStream fileStream = File.Create(text);
+					BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+					binaryWriter.Write(array.Length);
+					binaryWriter.Write(array);
+					binaryWriter.Flush();
+					fileStream.Flush(flushToDisk: true);
+					binaryWriter.Close();
+					binaryWriter.Dispose();
+					fileStream.Close();
+					fileStream.Dispose();
+					if (File.Exists(metaPath))
+					{
+						if (File.Exists(text2))
+						{
+							File.Delete(text2);
+							Thread.Sleep(1000);
+						}
+						File.Move(metaPath, text2);
+						Thread.Sleep(1000);
+					}
+					File.Move(text, metaPath);
+				}
+				catch (Exception ex)
+				{
+					ZLog.Log("SaveWorldMetaData EXCEPTION");
+					ZLog.LogError(ex.Message);
+					ZLog.LogError(ex.StackTrace);
+				}
+				return false;
+			}
+		}
+
+		public static SyncedList fresh;
+
+		public static SyncedList classes;
+
+		public static string playerClass = "";
+
+		public static Dictionary<string, int> timers = new Dictionary<string, int>();
+
+		public static void RPC_GetClass(long sender, string playername)
+		{
+			string text = "";
+			if (!(ZNet.instance != null) || !ZNet.instance.IsServer() || string.IsNullOrWhiteSpace(playername))
+			{
+				return;
+			}
+			ZNetPeer peerByPlayerName = ZNet.instance.GetPeerByPlayerName(playername);
+			if (!(peerByPlayerName?.IsReady() ?? false))
+			{
+				return;
+			}
+			List<string> list = classes.GetList();
+			foreach (string item in list)
+			{
+				string[] array = item.Split('|');
+				if (array[0] == peerByPlayerName.m_socket.GetHostName())
+				{
+					RPC_ModerationLog(0L, peerByPlayerName.m_socket.GetHostName() + "|" + peerByPlayerName.m_playerName + " sent class " + array[1]);
+					text = array[1];
+				}
+			}
+			ZRoutedRpc.instance.InvokeRoutedRPC(peerByPlayerName.m_uid, "ClientSetClass", text);
+		}
+
+		public static void RPC_AddFresh(long sender, string playername)
+		{
+			ZLog.Log("RPC_AddFresh " + playername);
+			if (ZNet.instance != null && ZNet.instance.IsServer() && !string.IsNullOrWhiteSpace(playername))
+			{
+				ZNetPeer peerByPlayerName = ZNet.instance.GetPeerByPlayerName(playername);
+				if (peerByPlayerName?.IsReady() ?? false)
+				{
+					string text = peerByPlayerName.m_socket.GetHostName() + "|" + peerByPlayerName.m_playerName;
+					fresh.Add(text);
+					RPC_ModerationLog(0L, text + " added to fresh list");
+				}
+			}
+		}
+
+		public static void RPC_ModerationLog(long sender, string msg)
+		{
+			if (ZNet.instance != null && ZNet.instance.IsServer())
+			{
+				string path = Utils.GetSaveDataPath() + "/moderation.txt";
+                using StreamWriter streamWriter = new StreamWriter(path, append: true);
+				streamWriter.WriteLine(DateTime.Now.ToUniversalTime().ToString() + " " + msg);
+			}
+		}
+	}
+}
