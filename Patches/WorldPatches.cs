9af35de102892b536322ea7da7f2bec4ff3416ea
AzumattDev <azumattgaming@gmail.com>
2021-09-20 04:00:28 -0400

    Update to InventoryGUI.cs to remove IL code and replace with simpler patch. Attempt to fix container size not changing for chests. Not sure why it's not working for WoodChests.

--- Patches/WorldPatches.cs
+++ Patches/WorldPatches.cs
@@ -8,6 +8,7 @@ namespace VMP_Mod.Patches
         [HarmonyPatch(typeof(DungeonGenerator), "Generate", typeof(int), typeof(ZoneSystem.SpawnMode))]
         private static void ApplyGeneratorSettings(ref DungeonGenerator __instance)
         {
+            
             __instance.m_minRooms = 40;
             __instance.m_maxRooms = 70;
             __instance.m_campRadiusMin = 40f;
