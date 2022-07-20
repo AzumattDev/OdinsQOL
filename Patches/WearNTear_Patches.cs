a4a8729824a1d66d80c6368a415f92801f285d07
AzumattDev <azumattgaming@gmail.com>
2022-07-19 21:16:09 -0400

    AlphaNovaLight's request to update Integrity

--- Patches/WearNTear_Patches.cs
+++ Patches/WearNTear_Patches.cs
@@ -9,7 +9,9 @@ namespace OdinQOL.Patches
     {
         public static ConfigEntry<bool> NoWeatherDam = null!;
         public static ConfigEntry<bool> DisableStructintegrity = null!;
+        public static ConfigEntry<bool> StructuralIntegrityControl = null!;
         public static ConfigEntry<bool> DisableBoatDamage = null!;
+        public static ConfigEntry<bool> DisableBoatWaterDamage = null!;
         public static ConfigEntry<bool> NoPlayerStructDam = null!;
         public static ConfigEntry<float> StructuralIntegritywood = null!;
         public static ConfigEntry<float> StructuralIntegritystone = null!;
@@ -60,18 +62,25 @@ namespace OdinQOL.Patches
         private static bool Prefix(ref WearNTear __instance, ref float damage)
         {
             // Gets the name of the method calling the ApplyDamage method
-            StackTrace? stackTrace = new();
-            string? callingMethod = stackTrace.GetFrame(2).GetMethod().Name;
+            StackTrace stackTrace = new();
+            string callingMethod = stackTrace.GetFrame(2).GetMethod().Name;
 
-            if (!(WearNTear_Patches.NoPlayerStructDam.Value && __instance.m_piece &&
+            if (!(WearNTear_Patches.StructuralIntegrityControl.Value && __instance.m_piece &&
                   __instance.m_piece.IsPlacedByPlayer() && callingMethod != "UpdateWear"))
                 return true;
 
-            if (!__instance.m_piece.m_name.StartsWith("$ship", StringComparison.Ordinal))
-                return !WearNTear_Patches.DisableStructintegrity.Value;
-            if (stackTrace.GetFrame(15).GetMethod().Name == "UpdateWaterForce") return false;
+            if (__instance.m_piece.m_name.StartsWith("$ship", StringComparison.Ordinal))
+            {
+                return !WearNTear_Patches.DisableBoatDamage.Value && (!WearNTear_Patches.DisableBoatWaterDamage.Value ||
+                                                                      stackTrace.GetFrame(15).GetMethod().Name !=
+                                                                      "UpdateWaterForce");
+            }
 
-            return true;
+            if (!__instance.m_piece.m_name.StartsWith("$cart", StringComparison.Ordinal))
+                return !WearNTear_Patches.NoPlayerStructDam.Value;
+            return !WearNTear_Patches.NoPlayerStructDam.Value && (!WearNTear_Patches.NoPlayerStructDam.Value ||
+                                                                  stackTrace.GetFrame(15).GetMethod().Name !=
+                                                                  "UpdateWaterForce");
         }
     }
 
@@ -84,7 +93,7 @@ namespace OdinQOL.Patches
         private static bool Prefix(ref WearNTear __instance, out float maxSupport, out float minSupport,
             out float horizontalLoss, out float verticalLoss)
         {
-            if (WearNTear_Patches.DisableStructintegrity.Value)
+            if (WearNTear_Patches.DisableStructintegrity.Value && WearNTear_Patches.StructuralIntegrityControl.Value)
             {
                 maxSupport = 1500f;
                 minSupport = 20f;
@@ -93,46 +102,87 @@ namespace OdinQOL.Patches
                 return false;
             }
 
-            // This handling is choosen because we subtract from a value thats reduced by distance from ground contact.
-            WearNTear_Patches.StructuralIntegritywood.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
-                ? 100
-                : WearNTear_Patches.StructuralIntegritywood.Value;
-            WearNTear_Patches.StructuralIntegritystone.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
-                ? 100
-                : WearNTear_Patches.StructuralIntegritystone.Value;
-            WearNTear_Patches.StructuralIntegrityiron.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
-                ? 100
-                : WearNTear_Patches.StructuralIntegrityiron.Value;
-            WearNTear_Patches.StructuralIntegrityhardWood.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
-                ? 100
-                : WearNTear_Patches.StructuralIntegrityhardWood.Value;
+            if (WearNTear_Patches.StructuralIntegrityControl.Value)
+            {
+                // This handling is chosen because we subtract from a value that's reduced by distance from ground contact.
+                WearNTear_Patches.StructuralIntegritywood.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
+                    ? 100
+                    : WearNTear_Patches.StructuralIntegritywood.Value;
+                WearNTear_Patches.StructuralIntegritystone.Value =
+                    WearNTear_Patches.StructuralIntegritywood.Value >= 100
+                        ? 100
+                        : WearNTear_Patches.StructuralIntegritystone.Value;
+                WearNTear_Patches.StructuralIntegrityiron.Value = WearNTear_Patches.StructuralIntegritywood.Value >= 100
+                    ? 100
+                    : WearNTear_Patches.StructuralIntegrityiron.Value;
+                WearNTear_Patches.StructuralIntegrityhardWood.Value =
+                    WearNTear_Patches.StructuralIntegritywood.Value >= 100
+                        ? 100
+                        : WearNTear_Patches.StructuralIntegrityhardWood.Value;
+
+                switch (__instance.m_materialType)
+                {
+                    case WearNTear.MaterialType.Wood:
+                        maxSupport = 100f;
+                        minSupport = 10f;
+                        verticalLoss = 0.125f - ((0.125f / 100) * WearNTear_Patches.StructuralIntegritywood.Value);
+                        horizontalLoss = 0.2f - ((0.125f / 100) * WearNTear_Patches.StructuralIntegritywood.Value);
+                        return false;
+                    case WearNTear.MaterialType.Stone:
+                        maxSupport = 1000f;
+                        minSupport = 100f;
+                        verticalLoss = 0.125f - ((0.125f / 100) * WearNTear_Patches.StructuralIntegritystone.Value);
+                        horizontalLoss = 1f - ((1f / 100) * WearNTear_Patches.StructuralIntegritystone.Value);
+                        return false;
+                    case WearNTear.MaterialType.Iron:
+                        maxSupport = 1500f;
+                        minSupport = 20f;
+                        verticalLoss = 0.07692308f -
+                                       ((0.07692308f / 100) * WearNTear_Patches.StructuralIntegrityiron.Value);
+                        horizontalLoss = 0.07692308f -
+                                         ((0.07692308f / 100) * WearNTear_Patches.StructuralIntegrityiron.Value);
+                        return false;
+                    case WearNTear.MaterialType.HardWood:
+                        maxSupport = 140f;
+                        minSupport = 10f;
+                        verticalLoss = 0.1f - ((0.1f / 100) * WearNTear_Patches.StructuralIntegrityhardWood.Value);
+                        horizontalLoss = 0.16666667f -
+                                         ((0.16666667f / 100) * WearNTear_Patches.StructuralIntegrityhardWood.Value);
+                        return false;
+                    default:
+                        maxSupport = 0f;
+                        minSupport = 0f;
+                        verticalLoss = 0f;
+                        horizontalLoss = 0f;
+                        return false;
+                }
+            }
 
             switch (__instance.m_materialType)
             {
                 case WearNTear.MaterialType.Wood:
                     maxSupport = 100f;
                     minSupport = 10f;
-                    verticalLoss = 0.125f - 0.125f / 100 * WearNTear_Patches.StructuralIntegritywood.Value;
-                    horizontalLoss = 0.2f - 0.125f / 100 * WearNTear_Patches.StructuralIntegritywood.Value;
+                    verticalLoss = 0.125f;
+                    horizontalLoss = 0.2f;
                     return false;
                 case WearNTear.MaterialType.Stone:
                     maxSupport = 1000f;
                     minSupport = 100f;
-                    verticalLoss = 0.125f - 0.125f / 100 * WearNTear_Patches.StructuralIntegritystone.Value;
-                    horizontalLoss = 1f - 1f / 100 * WearNTear_Patches.StructuralIntegritystone.Value;
+                    verticalLoss = 0.125f;
+                    horizontalLoss = 1f;
                     return false;
                 case WearNTear.MaterialType.Iron:
                     maxSupport = 1500f;
                     minSupport = 20f;
-                    verticalLoss = 0.07692308f - 0.07692308f / 100 * WearNTear_Patches.StructuralIntegrityiron.Value;
-                    horizontalLoss = 0.07692308f - 0.07692308f / 100 * WearNTear_Patches.StructuralIntegrityiron.Value;
+                    verticalLoss = 0.07692308f;
+                    horizontalLoss = 0.07692308f;
                     return false;
                 case WearNTear.MaterialType.HardWood:
                     maxSupport = 140f;
                     minSupport = 10f;
-                    verticalLoss = 0.1f - 0.1f / 100 * WearNTear_Patches.StructuralIntegrityhardWood.Value;
-                    horizontalLoss = 0.16666667f -
-                                     0.16666667f / 100 * WearNTear_Patches.StructuralIntegrityhardWood.Value;
+                    verticalLoss = 0.1f;
+                    horizontalLoss = 0.16666667f;
                     return false;
                 default:
                     maxSupport = 0f;
--- Utilities/Configs/WNTPatchConfigs.cs
+++ Utilities/Configs/WNTPatchConfigs.cs
@@ -6,26 +6,37 @@ public class WNTPatchConfigs
 {
     internal static void Generate()
     {
+        WearNTear_Patches.StructuralIntegrityControl = OdinQOLplugin.context.config("WearNTear_Patches",
+            "Structural Integrity Control", false,
+            "Set to true to enable the Structural Integrity settings in this mod.");
         WearNTear_Patches.NoWeatherDam = OdinQOLplugin.context.config("WearNTear_Patches",
             "No Weather Damage to buildings", false,
             "No Weather Damage to buildings");
         WearNTear_Patches.DisableStructintegrity = OdinQOLplugin.context.config("WearNTear_Patches",
-            "Disable Structural Integrity system", false, "Disable Structural Integrity system");
+            "Disable Structural Integrity system", false,
+            "Disable Structural Integrity system. Allows for placement of things in the air, does not prevent building damage.");
         WearNTear_Patches.DisableBoatDamage =
             OdinQOLplugin.context.config("WearNTear_Patches", "Disable Boat Damage", false, "Disable Boat Damage");
+        WearNTear_Patches.DisableBoatWaterDamage =
+            OdinQOLplugin.context.config("WearNTear_Patches", "Disable Boat Water Damage", false,
+                "Disable Boat Water Damage");
         WearNTear_Patches.NoPlayerStructDam = OdinQOLplugin.context.config("WearNTear_Patches",
             "No Damage to player buildings", false,
             "No Damage to player buildings");
 
         WearNTear_Patches.StructuralIntegritywood = OdinQOLplugin.context.config("WearNTear_Patches",
             "Wood Structural Integrity",
-            1f, "Wood Structural Integrity");
+            0f,
+            "Wood Structural Integrity. Reduces the loss of structural integrity by distance by % less. The value 100 would result in disabled structural integrity over distance, does not allow for placement in free air without Disable Structural Integrity system.");
         WearNTear_Patches.StructuralIntegritystone = OdinQOLplugin.context.config("WearNTear_Patches",
-            "Stone Structural Integrity", 1f, "Stone Structural Integrity");
+            "Stone Structural Integrity", 0f,
+            "Stone Structural Integrity. Reduces the loss of structural integrity by distance by % less. The value 100 would result in disabled structural integrity over distance, does not allow for placement in free air without Disable Structural Integrity system.");
         WearNTear_Patches.StructuralIntegrityiron = OdinQOLplugin.context.config("WearNTear_Patches",
             "Iron Structural Integrity",
-            1f, "Iron Structural Integrity");
+            0f,
+            "Iron Structural Integrity. Reduces the loss of structural integrity by distance by % less. The value 100 would result in disabled structural integrity over distance, does not allow for placement in free air without Disable Structural Integrity system.");
         WearNTear_Patches.StructuralIntegrityhardWood = OdinQOLplugin.context.config("WearNTear_Patches",
-            "Hardwood Structural Integrity", 1f, "Hardwood Structural Integrity");
+            "Hardwood Structural Integrity", 0f,
+            "Hardwood Structural Integrity. Reduces the loss of structural integrity by distance by % less. The value 100 would result in disabled structural integrity over distance, does not allow for placement in free air without Disable Structural Integrity system.");
     }
 }
